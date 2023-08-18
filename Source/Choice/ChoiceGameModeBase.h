8ac8d64413205ceb0daec5bc7dd2f8ead2ee1c76
Benjamin Wilson <35935536+bennywilson@users.noreply.github.com>
2023-08-18 16:39:22 -0400

    Initial commit

---
+++ Source/Choice/ChoiceGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "ChoiceGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class CHOICE_API AChoiceGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
